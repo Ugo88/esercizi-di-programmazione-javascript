8f80db246ea83c9be7d517b0750fc5be76d4654a
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2018-10-08 21:06:51 +0200

    Update esercizi-stringhe-numeri/quanti-anni-ha.js

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -1,5 +1,12 @@
 /*
   Quanti anni ha?
-  Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
+  Scrivi un programma che prenda in input un anno di nascita e determini l'età
+  della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
 */
+
+var anno di nascita = '1988';
+var anno corrente = '2018';
+var età=anno corrente - anno di nascita;
+console.log('la persona ha'+età+'anni');
+console.log('ai 100 anni mancano' +(100 - età)+'anni');
