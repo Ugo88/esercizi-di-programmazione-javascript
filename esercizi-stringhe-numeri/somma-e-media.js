a699a9d1d60b71e05d2a3d817f2c02296e674fcc
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2018-10-12 16:12:51 +0200

    Test Somma

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,11 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+var n1 = 3;
+var n2= 40;
+var n3 = 75;
+var n4 = 86;
+var n5 = 110;
+
+console.log('la somma è ' + (n1 + n2 + n3 + n4 + n5));
+console.log('la media è ' + (n1 + n2 + n3 + n4 + n5)/5);
