600465e609982f3b6fd2e88a09f726918444021b
Vincenzo Pisano <vincenzo.pisano@snapper.it>
2018-10-18 18:09:42 +0200

    Update

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -3,3 +3,7 @@
   Scrivi un programma che prenda quattro numeri in input, stampi in output il maggiore e il minore.
   http://www.imparareaprogrammare.it
 */
+var a=4, b=4, c=6, d=19;
+
+console.log('il valore più grande è ' + Math.max(a,b,c,d));
+console.log('il valore più piccolo è ' + Math.min(a,b,c,d));
--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -10,3 +10,27 @@
   Se la scelta non è tra quelle elencate verrà stampata la scritta 'Dolce non disponibile' altrimenti verrà visualizzata la scelta effettuata 'Hai scelto il dolce X'.
   http://www.imparareaprogrammare.it
 */
+console.log('MENU:');
+console.log('1. Tiramisù');
+console.log('2. Torta della nonna');
+console.log('3. Cheesecake alla nutella');
+console.log('4. Macedonia');
+
+var scelta=1;
+
+switch (scelta){
+  case 1:
+  console.log('il cliente ordina Tiramisù');
+  break;
+  case 2:
+  console.log('il cliente ordina Torta della nonna');
+  break;
+  case 3:
+  console.log('il cliente ordina Cheesecake alla nutella');
+  break;
+  case 4:
+  console.log('il cliente ordina Macedonia');
+  break;
+  default:
+  console.log('Dolce non disponibile');
+}
