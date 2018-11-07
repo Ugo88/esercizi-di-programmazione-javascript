67391497444855444f51cdb47dd04a541a324f6f
Ugo88 <ugoalebranc@gmail.com>
2018-11-07 11:54:35 +0100

    Update esercizi-array/invertimi.js
    Update

--- esercizi-array/invertimi.js
+++ esercizi-array/invertimi.js
@@ -6,3 +6,21 @@
   Non utilizzare array di appoggio per inversione.
   http://www.imparareaprogrammare.it
 */
+var array = [1];
+array.length = 100;
+var i =0;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+function isZero(numb,posiz,array2){
+  return numb == 0;
+}
+for (i=0; i < array.length; i++){
+array[i]= getRandomIntInclusive(1, 50)
+}
+console.log(array);
+console.log(array.reverse());
