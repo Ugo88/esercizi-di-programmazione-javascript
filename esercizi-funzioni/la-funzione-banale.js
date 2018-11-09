317bf90d41eff2678558d9985c284b5f9535262f
Ugo88 <ugoalebranc@gmail.com>
2018-11-09 17:22:35 +0100

    Update Function
    9/11

--- esercizi-funzioni/la-funzione-banale.js
+++ esercizi-funzioni/la-funzione-banale.js
@@ -3,3 +3,16 @@
   Scrivi una funzione di uguaglianza che prenda in input due argomenti e restituisca 'Vero' se i due argomenti sono identici, altrimenti 'Falso'.
   http://www.imparareaprogrammare.it
 */
+function trueFalse (first,second){
+    if(first == second){
+          console.log('vero');
+    }
+    else{
+          console.log('falso');
+    }
+}
+
+var primo = prompt ('inserisci il primo argomento');
+var secondo = prompt ('inserisci il secondo argomento');
+
+trueFalse (primo,secondo)
--- esercizi-funzioni/primo-e-ultimo.js
+++ esercizi-funzioni/primo-e-ultimo.js
@@ -6,3 +6,17 @@
   Scrivi due funzioni che prendano in input un array di numeri, una funzione deve restituire il valore maggiore contenuto nell'array, l'altra il valore minore.
   http://www.imparareaprogrammare.it
 */
+
+function max(first,second,third){
+        console.log(Math.max(first,second,third));
+}
+function min(first,second,third){
+        console.log(Math.min(first,second,third));
+}
+
+var primo = prompt ('inserisci il primo numero ');
+var secondo = prompt ('inserisci il secondo numero');
+var terzo = prompt ('inserisci il terzo argomento');
+
+min(primo,secondo,terzo);
+max(primo,secondo,terzo)
