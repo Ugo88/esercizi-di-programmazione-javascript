6c722dbb1db8f67af5be3c98f64402d09088bfbd
Ugo88 <ugoalebranc@gmail.com>
2018-11-18 18:53:19 +0100

    Update primo-e-ultimo.js
    18/11

--- esercizi-funzioni/primo-e-ultimo.js
+++ esercizi-funzioni/primo-e-ultimo.js
@@ -7,6 +7,8 @@
   http://www.imparareaprogrammare.it
 */
 
+/*
+//primo esercizio
 function max(first,second,third){
         console.log(Math.max(first,second,third));
 }
@@ -20,3 +22,20 @@ var terzo = prompt ('inserisci il terzo argomento');
 
 min(primo,secondo,terzo);
 max(primo,secondo,terzo)
+*/
+
+//secondo esercizio
+var listanumeri =  [0,3,6];
+
+function maxarray(array){
+      return Math.max(...array);
+        }
+
+function minarray(array){
+      return Math.min(...array);
+        }
+var maximum = maxarray(listanumeri);
+var minimum = minarray(listanumeri);
+
+console.log(`il numero massimo dell'array è ${maximum}`);
+console.log(`il numero minimo dell'array è ${minimum}`);
